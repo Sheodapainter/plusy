a367d9dd237def078bdd76aa0000b142b73dc6f5
Sheodapainter <mgarbiec9@gmail.com>
2025-06-23 01:34:38 +0200

    Update Client.java
    korekta literówki w kodzie

--- kol22/src/main/java/org/example/Client.java
+++ kol22/src/main/java/org/example/Client.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class Client extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view.xml")); //ładuje plik z katalogu resources
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view.fxml")); //ładuje plik z katalogu resources
         Scene scene = new Scene(loader.load()); //wczytuje strukturę z view, tworzy komponenty i kontroler
         primaryStage.setTitle("Klient"); //tytuł okna
         primaryStage.setScene(scene); //przypisuje scenę do okna
